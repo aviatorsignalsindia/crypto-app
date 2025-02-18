43987e4699ff76f61b13fbbda62e60a0543dea0a
Mark Kamesnkikh <mark.kamenskikh@vk.com>
2025-02-18 17:23:41 +0300

    первая правка работает

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { Flex, Layout } from 'antd';
-const { Header, Footer, Sider, Content } = Layout;
+import { Layout } from 'antd';
+
 const headerStyle = {
   textAlign: 'center',
   color: '#fff',
@@ -7,43 +7,31 @@ const headerStyle = {
   paddingInline: 48,
   lineHeight: '64px',
   backgroundColor: '#4096ff',
-};
+}
 const contentStyle = {
   textAlign: 'center',
   minHeight: 120,
   lineHeight: '120px',
   color: '#fff',
   backgroundColor: '#0958d9',
-};
+}
 const siderStyle = {
   textAlign: 'center',
   lineHeight: '120px',
   color: '#fff',
   backgroundColor: '#1677ff',
-};
-const footerStyle = {
-  textAlign: 'center',
-  color: '#fff',
-  backgroundColor: '#4096ff',
-};
-const layoutStyle = {
-  borderRadius: 8,
-  overflow: 'hidden',
-  width: 'calc(50% - 8px)',
-  maxWidth: 'calc(50% - 8px)',
-};
+}
 
 export default function App() {
   return (   
-     <Layout style={layoutStyle}>
-    <Header style={headerStyle}>Header</Header>
+     <Layout>
+    <Layout.Header style={headerStyle}>Header</Layout.Header>
     <Layout>
-      <Sider width="25%" style={siderStyle}>
+      <Layout.Sider width="25%" style={siderStyle}>
         Sider
-      </Sider>
-      <Content style={contentStyle}>Content</Content>
+      </Layout.Sider>
+      <Layout.Content style={contentStyle}>Content</Layout.Content>
     </Layout>
-    <Footer style={footerStyle}>Footer</Footer>
   </Layout>
   )
 }
