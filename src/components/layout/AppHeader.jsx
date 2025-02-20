d464d7f21bdb0a30a2844025795efa2c0e169a67
Mark Kamesnkikh <mark.kamenskikh@vk.com>
2025-02-20 23:34:58 +0300

    1:12:26 красивые страны выпадают

--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -1,4 +1,4 @@
-import { Layout } from 'antd';
+import { Layout, Select, Space } from 'antd';
 
 const headerStyle = {
     width: '100%',
@@ -6,12 +6,79 @@ const headerStyle = {
     height: 60,
     padding: '1rem',
     display: 'flex',
+    background: 'white',
     justifyContent: 'space-between',
     alignItems: 'center',
   }
 
+  const handleChange = (value) => {
+    console.log(`selected ${value}`);
+  };
+  const options = [
+    {
+      label: 'China',
+      value: 'china',
+      emoji: '🇨🇳',
+      desc: 'China (中国)',
+    },
+    {
+      label: 'USA',
+      value: 'usa',
+      emoji: '🇺🇸',
+      desc: 'USA (美国)',
+    },
+    {
+      label: 'Japan',
+      value: 'japan',
+      emoji: '🇯🇵',
+      desc: 'Japan (日本)',
+    },
+    {
+      label: 'Korea',
+      value: 'korea',
+      emoji: '🇰🇷',
+      desc: 'Korea (韩国)',
+    },
+  ];
+  const App = () => (
+    <Select
+      mode="multiple"
+      style={{
+        width: '100%',
+      }}
+      placeholder="select one country"
+      defaultValue={['china']}
+      onChange={handleChange}
+      options={options}
+      optionRender={(option) => (
+        <Space>
+          <span role="img" aria-label={option.data.label}>
+            {option.data.emoji}
+          </span>
+          {option.data.desc}
+        </Space>
+      )}
+    />
+  );
+
 export default function AppHeader () {
 
-    return (<Layout.Header style={headerStyle}>Header</Layout.Header>)
+    return (<Layout.Header style={headerStyle}>
+      <Select
+    style={{ width: 250, }}
+    placeholder="select one country"
+    defaultValue={['china']}
+    onChange={handleChange}
+    options={options}
+    optionRender={(option) => (
+      <Space>
+        <span role="img" aria-label={option.data.label}>
+          {option.data.emoji}
+        </span>
+        {option.data.desc}
+      </Space>
+    )}
+  />
+    </Layout.Header>)
 
 }
